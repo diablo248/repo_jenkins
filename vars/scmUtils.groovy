a0aff5fd482c25a87d11cf0cb7e177ae0d6e4f33
diablo248 <mr.mkochanowski@gmail.com>
2023-03-07 13:22:39 +0100

    Update scmUtils.groovy

--- vars/scmUtils.groovy
+++ vars/scmUtils.groovy
@@ -1,4 +1,8 @@
 def gitCheckout(Map params) {
- 
-checkout scmGit(branches: [[name: params.branch]], extensions: [], userRemoteConfigs: [[credentialsId: params.credentials, url: params.url]])
+    checkout scmGit(branches: [[name: params.branch]], extensions: [], userRemoteConfigs: [[credentialsId: params.credentials, url: params.url]])
+}
+
+def setGitUserInfo((Map params) {
+  sh "git config --global user.name ${params.user}"
+  sh "git config --global user.email ${params.mail}"
 }
